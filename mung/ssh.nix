020c75c7249fe6c26d0f2b25deff84deacf71d8f
Noah Harvey <noah.harvey247@gmail.com>
2025-10-20 01:57:08 -0700

    adds lima to ssh config for mung

--- mung/ssh.nix
+++ mung/ssh.nix
@@ -1,17 +1,22 @@
 { flake.nixosModules.mung =
   { # Enable the OpenSSH daemon.
     services.openssh =
-      { enable = true;
-        settings =
-        { X11Forwarding = true;
-        };
-        extraConfig =
-        ''
-        Host lima
-          User noah
-          HostName 192.168.1.49
-        '';
+    { enable = true;
+      settings =
+      { X11Forwarding = true;
       };
+    };
+
+    ssh.extraConfig =
+    ''
+    Host lima
+      User noah
+      HostName 192.168.1.49
+    '';
+
+    networking.hosts =
+    { "192.168.1.49" = [ "lima" ];
+    };
 
   };
 }
