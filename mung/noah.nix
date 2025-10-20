30b991213bd6693ca5932599e1675e2a7ea2e82d
theNerd247 <noah.harvey247@gmail.com>
2025-10-20 08:36:45 +0000

    fixing bad noah.nix config

--- mung/noah.nix
+++ mung/noah.nix
@@ -1,7 +1,9 @@
-{ flake.nixosModule.mung = 
+{ flake.nixosModules.mung = 
   {
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.noah = {
+      enable = true;
+      home = /home/noah;
       isNormalUser = true;
       description = "noah";
       extraGroups = [ "networkmanager" "wheel" "tty" ];
