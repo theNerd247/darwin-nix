de01ae9080830faa07bb94766e997af14c018d58
theNerd247 <noah.harvey247@gmail.com>
2025-12-25 08:46:23 +0000

    enables avahi on mung

--- mung/network.nix
+++ mung/network.nix
@@ -1,17 +1,26 @@
-{ flake =
+{ flake.nixosModules.mung =
   { networking.hostName = "mung"; # Define your hostname.
     networking.hosts =
     { "192.168.1.49" = [ "lima" ];
     };
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Enables wireless support via wpa_supplicant.
+    # networking.wireless.enable = true;
 
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
     # Enable networking
-    networking.networkmanager.enable = true;
+    # networking.networkmanager.enable = true;
 
-    services.avahi.enable = true;
+    services.avahi =
+    { enable = true;
+      nssmdns4 = true;
+      publish =
+      { enable = true;
+        addresses = true;
+      };
+    };
   };
 }
