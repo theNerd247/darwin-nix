7d61570db4c622540402a33ff575e221ec063735
Noah Harvey <noah.harvey247@gmail.com>
2025-12-25 22:24:52 -0800

    nixos updates to 25.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Nix-Darwin configuration";
 
   inputs =
-  { nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  { nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
